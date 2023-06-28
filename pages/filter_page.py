b1a307d4e49f51918312acc0e48f2d595c41dd8c
AlisonDLovegood <alisondlovegood@gmail.com>
2023-06-28 00:44:57 -0300

    Descrição das consultas e novo gráfico de dispersão

--- pages/filter_page.py
+++ pages/filter_page.py
@@ -18,32 +18,46 @@ electric_pokemon = pokemon_df.loc[pokemon_df['typing'].str.contains('Electric'),
 normal_pokemon = pokemon_df.loc[pokemon_df['typing'].str.contains('Normal'), ['pokedex_number', 'name', 'typing', 'abilities', 'hp', 'can_evolve', 'shape']]
 
 st.subheader("Filtros do DF")
+st.text("Cada uma das opções irá retornar uma tabela distinta com base em um filtro de dados")
 option = st.selectbox(
-    'Selecione um dos filtros para mostrar os dados:',
+    'Selecione um dos filtros:',
     ['Informações Básicas', 'Informações Avançadas', 'Fire Pokémon', 'Water Pokémon', 
      'Ghost Pokémon', 'Grass Pokémon', 'Rock Pokémon', 'Ice Pokémon', 'Poison Pokémon', 
      'Electric Pokémon', 'Normal Pokémon'])
 
 # Exibe os dados correspondentes ao filtro selecionado
 if option == 'Informações Básicas':
+    st.text("Informações comuns referente a todos os pokémon")
     st.dataframe(basic_information)
 elif option == 'Informações Avançadas':
+    st.text("Informações mais complexas, porém não todas, referentes a todos os pokémon")
     st.dataframe(advanced_information)
 elif option == 'Fire Pokémon':
+    st.text("Informações fundamentais sobre os pokémon do tipo fogo")
     st.dataframe(fire_pokemon)
 elif option == 'Water Pokémon':
+    st.text("Informações fundamentais sobre os pokémon do tipo água")
     st.dataframe(water_pokemon)
 elif option == 'Ghost Pokémon':
+    st.text("Informações fundamentais sobre os pokémon do tipo fantasma")
     st.dataframe(ghost_pokemon)
 elif option == 'Grass Pokémon':
+    st.text("Informações fundamentais sobre os pokémon do tipo planta")
     st.dataframe(grass_pokemon)
 elif option == 'Rock Pokémon':
+    st.text("Informações fundamentais sobre os pokémon do tipo pedra")
     st.dataframe(rock_pokemon)
 elif option == 'Ice Pokémon':
+    st.text("Informações fundamentais sobre os pokémon do tipo gelo")
     st.dataframe(ice_pokemon)
 elif option == 'Poison Pokémon':
+    st.text("Informações fundamentais sobre os pokémon do tipo venenoso")
     st.dataframe(poison_pokemon)
 elif option == 'Electric Pokémon':
+    st.text("Informações fundamentais sobre os pokémon do tipo eletrico")
     st.dataframe(electric_pokemon)
 elif option == 'Normal Pokémon':
+    st.text("Informações fundamentais sobre os pokémon do tipo normal")
     st.dataframe(normal_pokemon)
+
+st.text("Vale ressaltar que as consultas não estão buscando apenas os pokémon que possuem UM UNICO tipo!")
--- pages/graph_page.py
+++ pages/graph_page.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 pokemon_df = pd.read_csv("data\pokemon.csv")
 # CODIGO QUE RETIRA OS DUPICADAS DE NÚMERO DA POKEDEX
@@ -10,14 +11,32 @@ format_information = pokemon_df['shape'].value_counts()
 gen_information = pokemon_df['gen_introduced'].value_counts()
 
 st.subheader("Filtros do DF")
+st.text("Cada uma das opções irá retornar um gráfico distinto com base em um filtro de dados")
 option = st.selectbox(
     'Selecione um dos filtros para mostrar os dados:',
-    ['Quantidade de espécies por tipo', 'Quantidade de espécies por formato', 'Quantidade de espécies por geração'])
+    ['Quantidade de espécies por tipo', 'Quantidade de espécies por formato', 'Quantidade de espécies por geração', 'Gráfico de dispersão'])
 
 # Exibe os dados correspondentes ao filtro selecionado
 if option == 'Quantidade de espécies por tipo':
+    st.text("Apresenta um gráfico contando a quantidade de pokémon por cada tipo existente")
+    st.text("Ele inclui os pokemon que possuem dois tipos, e os subdivide")
+    st.markdown("<br>", unsafe_allow_html=True)
     st.line_chart(type_information)
 elif option == 'Quantidade de espécies por formato':
+    st.text("Apresenta um gráfico que contabiliza a quantidade de pokemon definidos pelo formato")
+    st.markdown("<br>", unsafe_allow_html=True)
     st.line_chart(format_information)
 elif option == 'Quantidade de espécies por geração':
+    st.text("Gráfico que explicita nitidamente a quantidade de pokemon lançadas por geração")
+    st.markdown("<br>", unsafe_allow_html=True)
     st.bar_chart(gen_information.sort_index())
+elif option == 'Gráfico de dispersão':
+    st.text("Gráfico de dispersão que relaciona altura e peso dos pokemon")
+    st.text("Utilizando a biblioteca matplotlib")
+    st.markdown("<br>", unsafe_allow_html=True)
+    fig, ax = plt.subplots()
+    ax.scatter(pokemon_df['height'], pokemon_df['weight'])
+    ax.set_xlabel('Altura')
+    ax.set_ylabel('Peso')
+    ax.set_title('Relação entre Altura e Peso dos Pokémon')
+    st.pyplot(fig)
